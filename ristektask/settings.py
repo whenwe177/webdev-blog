f224a814c1a0f622ee90e99ccf5796f445eb921d
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-03-09 11:22:08 +0700

    Prepare for heroku deploy

--- ristektask/settings.py
+++ ristektask/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wd8-h!xog#bj0bxucth#bsvrl(kisksv=+16q^)(x4n14s*n06'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["webdev-blog-alexandercb.herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -120,7 +122,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Activate Django-Heroku
+django_heroku.settings(locals())
