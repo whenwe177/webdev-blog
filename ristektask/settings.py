cdeeeeb554a54483be19700c84e5031b2f2e4571
alexander.caesario <alexandercaesariobangun@gmail.com>
2022-03-11 17:38:27 +0700

    Prepare for heroku deploy

--- ristektask/settings.py
+++ ristektask/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,15 +89,6 @@ DATABASES = {
     }
 }
 
-if "DATABASE_URL" in os.environ:
-    import dj_database_url
-
-    DATABSES = {
-        "default" : dj_database_url.config()
-    }
-
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -139,3 +131,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku
+django_heroku.settings(locals())
